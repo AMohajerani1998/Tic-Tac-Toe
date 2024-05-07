2c951d9f0cc3a014214474f210fdb8340a0d54cd
AMohajerani1998 <ashkan.mohajerani1998@gmail.com>
2024-05-07 23:52:56 +0200

    created new CSS class for every player's turn for winning player's js feature

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ function tileClick(event) {
     targetContent = event.target.innerHTML;
     if (e % 2 == 0) {
         if (targetContent == "") {
-            event.target.classList.add('marked')
+            event.target.classList.add("marked1");
             event.target.innerHTML = "<div>X</div>";
         }
     } else {
         if (targetContent == "") {
-            event.target.classList.add('marked')
+            event.target.classList.add("marked2");
             event.target.innerHTML = "<div>O</div>";
         }
     }
+    const markedTiles = document.querySelectorAll('marked');
+
 }
 let i = 0;
 for (tile of gameTiles) {
@@ -22,3 +24,12 @@ for (tile of gameTiles) {
     //tile.firstElementChild.value = i;
     tile.addEventListener("click", tileClick);
 }
+
+const newGameButton = document.getElementById("new-game-button");
+function newGame() {
+    for (tile of gameTiles) {
+        tile.innerHTML = "";
+        tile.classList = "game-tile";
+    }
+}
+newGameButton.addEventListener("click", newGame);
