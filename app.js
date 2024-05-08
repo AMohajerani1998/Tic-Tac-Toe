6e315b851844d39636a04fb2dc60491584f07070
AMohajerani1998 <ashkan.mohajerani1998@gmail.com>
2024-05-08 12:02:02 +0200

    reverted the changes done to app.js

--- app.js
+++ app.js
@@ -18,52 +18,7 @@ function tileClick(event) {
     const markedTilesPlayer2 = document.querySelectorAll(".marked2");
     player1TilesLength = markedTilesPlayer1.length;
     player2TilesLength = markedTilesPlayer2.length;
-    for (let a = 0; a <= player1TilesLength; a++) {
-        if (
-            (markedTilesPlayer1[a - 1].value ==
-                markedTilesPlayer1[a].value + 1 &&
-                markedTilesPlayer1[a].value ==
-                    markedTilesPlayer1[a + 1].value + 1) ||
-            (markedTilesPlayer1[a - 1].value ==
-                markedTilesPlayer1[a].value + 2 &&
-                markedTilesPlayer1[a].value ==
-                    markedTilesPlayer1[a + 1].value + 2) ||
-            (markedTilesPlayer1[a - 1].value ==
-                markedTilesPlayer1[a].value + 3 &&
-                markedTilesPlayer1[a].value ==
-                    markedTilesPlayer1[a + 1].value + 3) ||
-            (markedTilesPlayer1[a - 1].value ==
-                markedTilesPlayer1[a].value + 4 &&
-                markedTilesPlayer1[a].value ==
-                    markedTilesPlayer1[a + 1].value + 4)
-        ) {
-            console.log("player1 wins");
-        }
-    }
-    for (let k = 0; k <= player2TilesLength; k++) {
-        console.log(markedTilesPlayer1)
-        if (
-            (markedTilesPlayer2[k - 1].value ==
-                markedTilesPlayer2[k].value + 1 &&
-                markedTilesPlayer2[k].value ==
-                    markedTilesPlayer2[k + 1].value + 1) ||
-            (markedTilesPlayer2[k - 1].value ==
-                markedTilesPlayer2[k].value + 2 &&
-                markedTilesPlayer2[k].value ==
-                    markedTilesPlayer2[k + 1].value + 2) ||
-            (markedTilesPlayer2[k - 1].value ==
-                markedTilesPlayer2[k].value + 3 &&
-                markedTilesPlayer2[k].value ==
-                    markedTilesPlayer2[k + 1].value + 3) ||
-            (markedTilesPlayer2[k - 1].value ==
-                markedTilesPlayer2[k].value + 4 &&
-                markedTilesPlayer2[k].value ==
-                    markedTilesPlayer2[k + 1].value + 4)
-        ) {
-            console.log("test");
-            console.log("player2 wins");
-        }
-    }
+    console.log("test");
 }
 let i = 0;
 for (tile of gameTiles) {
